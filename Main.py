f6db287b82f0fe1b3a95da07457ca0a12f4602c3
KorvinPrim <shashatkin@mail.ru>
2018-04-16 00:01:58 +0400

    Commit 5, for 16.04.2018, create help journey

--- Main.py
+++ Main.py
@@ -5,7 +5,33 @@ import sys
 import requests
 import time
 import wikipedia
+
 wikipedia.set_lang('ru')
+
+
+class Help_journey:
+    def __init__(self):
+        try:
+            self.advice = {}
+            link = requests.get('http://russoturista.ru/poleznaya-informaciya/za-granicu-vpervye/')
+            for i in link.text.split('<h2 style="text-align: justify;">')[1:-1]:
+                n = \
+                    i.replace('<p style="text-align: justify;">', '').replace('</p>', '').replace('&#8212;',
+                                                                                                  '').replace(
+                        '\n', '').replace('<br />', '').replace(
+                        '<p><span style="color: #555555; font-family: Ubuntu, sans-serif; font-size: 20px; line-height: 55px; text-align: justify;">Подбор тура</span>',
+                        '').split('<a href=')[0].split('</h2>')
+                self.advice[str(n[0]).strip()] = n[1]
+        except:
+            self.advice = False
+
+    def get_jorney(self, mess):
+        if self.advice:
+            return self.advice[mess] if mess in self.advice else 'Хотите выйти?'
+
+
+help = Help_journey()
+
 
 class Get_link:
     def geocoder_request(self, **kwargs):
@@ -43,7 +69,7 @@ class Get_link:
             print("Запрос не удалось выполнить. Проверьте наличие сети Интернет.")
             sys.exit(1)
 
-    def geocode(self,address):
+    def geocode(self, address):
         geocoder_request = "http://geocode-maps.yandex.ru/1.x/?geocode={address}&format=json".format(**locals())
         response = requests.get(geocoder_request)
         if response:
@@ -58,7 +84,7 @@ class Get_link:
 
         return features[0]["GeoObject"] if features else None
 
-    def get_coordinates(self,address):
+    def get_coordinates(self, address):
         toponym = self.geocode(address)
         if not toponym:
             return False
@@ -67,18 +93,28 @@ class Get_link:
         toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
         return [float(toponym_longitude), float(toponym_lattitude)]
 
-    def return_link(self,inquiry):
+    def return_link(self, inquiry):
         coord = self.get_coordinates(inquiry)
         try:
             return self.link(ll='{0},{1}'.format(str(coord[0]), str(coord[1])),
-                                                    z=5, l='sat,skl')
+                             z=5, l='sat,skl')
         except Exception:
             return self.link(ll='{0},{1}'.format(str(0.0), str(0.0)),
                              z=0, l='sat,skl')
 
 
+def clear_chat_data(bot, update, chat_data):
+    del chat_data['climate']
+    del chat_data['type_of_rest']
+    del chat_data['money']
+    del chat_data['visa']
+    del chat_data['children']
+    del chat_data['return_lend']
+    del chat_data['help_journey']
+
+
 def return_data_to_lend(bot, update, chat_data):
-    continuation = [['Получить помощь по путешествию'], ['Особенности страны']]
+    continuation = [['Получить помощь по путешествию'], ['Особенности страны'], ['Новая страна']]
     continuation_markup = ReplyKeyboardMarkup(continuation, one_time_keyboard=True)
 
     countries = [[[[['Катар', 'Вьетнам'], ['Объединённые Арабские Эмираты', 'Бахрейн'], ['Мексика', 'Ямайка']],
@@ -109,6 +145,7 @@ def return_data_to_lend(bot, update, chat_data):
     except Exception:
         update.message.reply_text('Картинка недоступна.')
     update.message.reply_text('По данным cайта wikipedia.org: ' + wikipedia.summary(lend))
+    update.message.reply_text('Если хотите изменить свой выбор, скажите: Новая страна')
 
 
 def data(bot, update):
@@ -122,7 +159,40 @@ def echo(bot, update, chat_data):
 
 def calibration(bot, update, chat_data):
     print(update.message.text)
+
+    continuation = [['Получить помощь по путешествию'], ['Особенности страны'], ['Новая страна']]
+    continuation_markup = ReplyKeyboardMarkup(continuation, one_time_keyboard=True)
+
+    help_chnge = [
+        ['Первая поездка за границу  советы'],
+        ['Покупка тура'],
+        ['Вылет: таможенный и паспортный контроль, регистрация на рейс'],
+        ['Таможенный контроль'],
+        ['Сдача багажа и регистрация на рейс'],
+        ['Паспортный контроль'],
+        ['Аэропорт «пункта назначения»'],
+        ['Инфо-встреча'],
+        ['Назад']
+    ]
+
+    help_chnge_markup = ReplyKeyboardMarkup(help_chnge, one_time_keyboard=True)
+
     if update.message.text != '/start':
+        if 'return_lend' in chat_data:
+            if str(update.message.text).lower() == 'новая страна':
+                clear_chat_data(bot, update, chat_data)
+                update.message.reply_text('Начинаю опрос заново.')
+            else:
+                if 'help_journey' in chat_data:
+                    if update.message.text == 'Назад':
+                        del chat_data['help_journey']
+                        update.message.reply_text('Хотите ли вы что нибудь ещё?', reply_markup=continuation_markup)
+                    else:
+                        update.message.reply_text(help.get_jorney(update.message.text), reply_markup=help_chnge_markup)
+                else:
+                    update.message.reply_text('Если хотите пройти опрос заново скажите: Новая страна.',
+                                              reply_markup=continuation_markup)
+
         climat = [['Жаркий', 'Умеренный']]
         type_of_rest = [['Экей тюленик на пляжу', 'Исследователь'], ['Культурный гуру', 'Венера fm']]
         money = [['Дайте хлеба', 'Ммм... маслоу'], ['Неплохой айфон', 'Налог?.. пффф.']]
@@ -165,8 +235,14 @@ def calibration(bot, update, chat_data):
             children_mak = ReplyKeyboardMarkup(children, one_time_keyboard=True)
             update.message.reply_text('Будите ли вы брать в путешествие детей?', reply_markup=children_mak)
 
+        if update.message.text == 'Получить помощь по путешествию':
+            update.message.reply_text('Рекоминдации составлены сайтом: russoturista.ru', reply_markup=help_chnge_markup)
+            chat_data['help_journey'] = 'True'
+
     if 'children' in chat_data:
-        return_data_to_lend(bot, update, chat_data)
+        if 'return_lend' not in chat_data:
+            return_data_to_lend(bot, update, chat_data)
+        chat_data['return_lend'] = 'True'
 
     print(chat_data)
 
