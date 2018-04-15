d8c33208e7598bc59d76c43160a50103f4b2f275
KorvinPrim <shashatkin@mail.ru>
2018-04-15 21:51:05 +0400

    Commit 4, for 15.04.2018, return Picture, return info lend.

--- Main.py
+++ Main.py
@@ -1,6 +1,114 @@
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+import json
+import sys
+import requests
 import time
+import wikipedia
+wikipedia.set_lang('ru')
+
+class Get_link:
+    def geocoder_request(self, **kwargs):
+        try:
+            post = []
+            for key, val in kwargs.items():
+                post.append(key + '=' + val)
+            print(post)
+            address = 'http://geocode-maps.yandex.ru/1.x/?' + '&'.join(post)
+
+            response = requests.get(address)
+
+            if not response:
+                print('Ошибка выполнения запроса:')
+                print('Http статус: {} ({})'.format(response.status_code, response.reason))
+                sys.exit(1)
+
+            return json.loads(response.text)
+
+        except:
+            print('Запрос не удалось выполнить. Проверьте наличие сети Интернет.')
+            sys.exit(1)
+
+    def link(self, **kwargs):
+        try:
+            post = []
+            for key, val in kwargs.items():
+                post.append(str(key) + '=' + str(val))
+
+            map_request = "http://static-maps.yandex.ru/1.x/?{}".format('&'.join(post))
+
+            return map_request
+
+        except:
+            print("Запрос не удалось выполнить. Проверьте наличие сети Интернет.")
+            sys.exit(1)
+
+    def geocode(self,address):
+        geocoder_request = "http://geocode-maps.yandex.ru/1.x/?geocode={address}&format=json".format(**locals())
+        response = requests.get(geocoder_request)
+        if response:
+            json_response = response.json()
+        else:
+            raise RuntimeError(
+                """Ошибка выполнения запроса:
+                {request}
+                Http статус: {status} ({reason})""".format(
+                    request=geocoder_request, status=response.status_code, reason=response.reason))
+        features = json_response["response"]["GeoObjectCollection"]["featureMember"]
+
+        return features[0]["GeoObject"] if features else None
+
+    def get_coordinates(self,address):
+        toponym = self.geocode(address)
+        if not toponym:
+            return False
+        toponym_coodrinates = toponym["Point"]["pos"]
+
+        toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+        return [float(toponym_longitude), float(toponym_lattitude)]
+
+    def return_link(self,inquiry):
+        coord = self.get_coordinates(inquiry)
+        try:
+            return self.link(ll='{0},{1}'.format(str(coord[0]), str(coord[1])),
+                                                    z=5, l='sat,skl')
+        except Exception:
+            return self.link(ll='{0},{1}'.format(str(0.0), str(0.0)),
+                             z=0, l='sat,skl')
+
+
+def return_data_to_lend(bot, update, chat_data):
+    continuation = [['Получить помощь по путешествию'], ['Особенности страны']]
+    continuation_markup = ReplyKeyboardMarkup(continuation, one_time_keyboard=True)
+
+    countries = [[[[['Катар', 'Вьетнам'], ['Объединённые Арабские Эмираты', 'Бахрейн'], ['Мексика', 'Ямайка']],
+                   [['Малазия', 'Турция'], ['Тайланд', 'Сейшелы'], ['Мексика', 'Албания']],
+                   [['Багамы', 'Зимбабве'], ['Катар', 'Кения'], ['Куба', 'Мальдивы']],
+                   [['Мальта', 'Мексика'], ['Сейшелы', 'Тунис'], ['Боствана', 'Объединённые Арабские Эмираты']]],
+                  [[['Ямайка', 'Индия'], ['Бахрейн', 'Катар'], ['Аруба', 'Непал']],
+                   [['Гаити', 'Южный Судан'], ['Нигерия', 'Демократическая Республика Конго'],
+                    ['Камерун', 'Кения']],
+                   [['Гаити', 'Марокко'], ['Куба', 'Мальдивы'], ['Сейшелы', 'Объединённые Арабские Эмираты']],
+                   [['Австрия', 'Андорра'], ['Бразилия', 'Перу'], ['Тунис', 'Исландия']]]], [
+                     [[['Англия', 'Ирландия'], ['Люксембург', 'Германия'], ['Италия', 'Финляндия']],
+                      [['Австралия', 'Австрия'], ['Бельгия', 'Великобритания'], ['Германия', 'Ирландия']],
+                      [['Лихтенштейн', 'Люксембург'], ['Нидерланды', 'Франция'], ['Швейцария', 'Белоруссия']],
+                      [['Болгария', 'Венгрия'], ['Молдавия', 'Польша'], ['Россия', 'Румыния']]],
+                     [[['Словакия', 'Чехия'], ['Украина', 'Дания'], ['Исландия', 'Латвия']],
+                      [['Литва', 'Норвегия'], ['Финляндия', 'Эстония'], ['Швеция', 'Албания']],
+                      [['Андорра', 'Босния и Герцеговина'], ['Ватикан', 'Греция'], ['Испания', 'Италия']],
+                      [['Македония', 'Мальта'], ['Португалия', 'Сан-Марино'], ['Новая Зеландия', 'Руанда ']]]]]
+
+    lend = str(countries[chat_data['climate']][chat_data['type_of_rest']]
+               [chat_data['money']][chat_data['visa']][
+                   chat_data['children']])
+
+    update.message.reply_text('Мы рекомендуем вам посетить страну: ' + lend, reply_markup=continuation_markup)
+    try:
+        bot.send_photo(update.message.chat_id, Get_link().return_link(lend))
+    except Exception:
+        update.message.reply_text('Картинка недоступна.')
+    update.message.reply_text('По данным cайта wikipedia.org: ' + wikipedia.summary(lend))
 
 
 def data(bot, update):
@@ -24,7 +132,7 @@ def calibration(bot, update, chat_data):
         if ('climate' not in chat_data) and (update.message.text in climat[0]):
             chat_data['climate'] = climat[0].index(update.message.text)
         if ('type_of_rest' not in chat_data) and (
-                update.message.text in type_of_rest[0] or update.message.text in type_of_rest[1]):
+                        update.message.text in type_of_rest[0] or update.message.text in type_of_rest[1]):
             chat_data['type_of_rest'] = (type_of_rest[0].index(update.message.text) if update.message.text in
                                                                                        type_of_rest[0] else
                                          type_of_rest[1].index(update.message.text)) % 2
@@ -58,30 +166,7 @@ def calibration(bot, update, chat_data):
             update.message.reply_text('Будите ли вы брать в путешествие детей?', reply_markup=children_mak)
 
     if 'children' in chat_data:
-        continuation = [['Получить помощь по путешествию', 'Особенности страны']]
-        continuation_markup = ReplyKeyboardMarkup(continuation, one_time_keyboard=True)
-
-        countries = [[[[['Катар', 'Вьетнам'], ['Объединённые Арабские Эмираты', 'Бахрейн'], ['Мексика', 'Ямайка']],
-                       [['Малазия', 'Турция'], ['Тайланд', 'Сейшелы'], ['Мексика', 'Албания']],
-                       [['Багамы', 'Зимбабве'], ['Катар', 'Кения'], ['Куба', 'Мальдивы']],
-                       [['Мальта', 'Мексика'], ['Сейшелы', 'Тунис'], ['Боствана', 'Объединённые Арабские Эмираты']]],
-                      [[['Ямайка', 'Индия'], ['Бахрейн', 'Катар'], ['Аруба', 'Непал']],
-                       [['Гаити', 'Южный Судан'], ['Нигерия', 'Демократическая Республика Конго'],
-                        ['Камерун', 'Кения']],
-                       [['Гаити', 'Марокко'], ['Куба', 'Мальдивы'], ['Сейшелы', 'Объединённые Арабские Эмираты']],
-                       [['Австрия', 'Андорра'], ['Бразилия', 'Перу'], ['Тунис', 'Исландия']]]], [
-                         [[['Англия', 'Ирландия'], ['Люксембург', 'Германия'], ['Италия', 'Финляндия']],
-                          [['Австралия', 'Австрия'], ['Бельгия', 'Великобритания'], ['Германия', 'Ирландия']],
-                          [['Лихтенштейн', 'Люксембург'], ['Нидерланды', 'Франция'], ['Швейцария', 'Белоруссия']],
-                          [['Болгария', 'Венгрия'], ['Молдавия', 'Польша'], ['Россия', 'Румыния']]],
-                         [[['Словакия', 'Чехия'], ['Украина', 'Дания'], ['Исландия', 'Латвия']],
-                          [['Литва', 'Норвегия'], ['Финляндия', 'Эстония'], ['Швеция', 'Албания']],
-                          [['Андорра', 'Босния и Герцеговина'], ['Ватикан', 'Греция'], ['Испания', 'Италия']],
-                          [['Македония', 'Мальта'], ['Португалия', 'Сан-Марино'], ['Новая Зеландия', 'Руанда ']]]]]
-
-        update.message.reply_text('Мы рекомендуем вам посетить страну: ' + str(
-            countries[chat_data['climate']][chat_data['type_of_rest']][chat_data['money']][chat_data['visa']][
-                chat_data['children']]), reply_markup=continuation_markup)
+        return_data_to_lend(bot, update, chat_data)
 
     print(chat_data)
 
